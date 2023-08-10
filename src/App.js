a9acfd4a1b6cae8ede973702d8da29f4af7f865b
Thais <thaisplaneta2017@gmail.com>
2023-08-10 12:19:53 -0300

    testando o header no app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-// import Login from './pagina/Login/Login';
-import Register from './pagina/Register/Register';
-
 import StyledGlobal from "./styledGlobal";
 import { ChakraProvider } from '@chakra-ui/react'
 
-
+// import Login from './pagina/Login/Login';
+// import Register from './pagina/Register/Register';
+import Header from './componentes/Header/Header';
 
 function App() {
   return (
     <>
       <ChakraProvider>
         <StyledGlobal />
-        <Register />
+        <Header/>
+        {/* <Register /> */}
         {/* <Login/> */}
       </ChakraProvider>
     </>
