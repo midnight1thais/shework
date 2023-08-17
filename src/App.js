8fec013078c5dbf27d27a0fab060bcc28e434948
Thais <thaisplaneta2017@gmail.com>
2023-08-17 10:49:43 -0300

    Home

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { ChakraProvider } from '@chakra-ui/react'
 // import Login from './pagina/Login/Login';
 import Register from './pagina/Register/Register';
 import Header from './componentes/Header/Header';
-import Cover from './componentes/Cover/Cover'
+import Cover from './componentes/HomeCover/HomeCover'
 
 function App() {
   return (
     <>
       <ChakraProvider>
         <StyledGlobal />
-        {/* <Header/>
-        <Cover/> */}
-        <Register />
+        <Header/>
+        <Cover/>
+        {/* <Register /> */}
         {/* <Login/> */}
       </ChakraProvider>
     </>
