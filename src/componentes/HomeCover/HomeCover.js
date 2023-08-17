3de341e516bb45cbad8af2321e468d6bfa433e53
Thais <thaisplaneta2017@gmail.com>
2023-08-17 10:47:37 -0300

    Arrumei titulo

---
+++ src/componentes/HomeCover/HomeCover.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Background, Description, Title, TitleDiv } from './style';
+
+
+function Cover() {
+    return(
+        <Background>
+            <TitleDiv>
+                <Title>Empregando</Title>
+                <Title>Mulheres</Title>
+                
+                <Description>
+                    Aqui buscamos auxiliar na busca por 
+                </Description>
+                <Description>
+                    empregos tercerizados e dentro da TI
+                </Description>
+            </TitleDiv>
+
+        </Background>
+    )
+}
+
+export default Cover
