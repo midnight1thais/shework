6e0d100b2722dfadc36c7f831465db6c0f1d3a37
Thais <thaisplaneta2017@gmail.com>
2023-08-18 11:34:03 -0300

    Iniciei a criação da área das empresas

---
+++ src/componentes/HomeCompany/HomeCompany.js
@@ -0,0 +1,17 @@
+import { ContainerCarousel, ContainerCompany, ContainerPubli, Title } from "./style";
+
+function HomeCompany(){ 
+    return(
+        <ContainerCompany>
+            <Title>Faça parte da nossa rede de oportunidades</Title>
+            <ContainerCarousel>
+                carrossel
+            </ContainerCarousel>
+            <ContainerPubli>
+                publicações
+            </ContainerPubli>
+        </ContainerCompany>
+    )
+}
+
+export default HomeCompany
---
+++ src/componentes/HomeCompany/style.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const ContainerCompany = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    background-color: blue;
+`
+
+export const Title = styled.h1`
+    color: red;
+
+`
+
+export const ContainerCarousel = styled.div`
+    display: flex;
+`
+
+export const ContainerPubli = styled.div`
+   display: flex; 
+`
