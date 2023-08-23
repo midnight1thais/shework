72122b75ce85a8bb11255b15860d6f92f918cf52
Thais <thaisplaneta2017@gmail.com>
2023-08-22 23:52:52 -0300

    testando o carousel

--- src/componentes/HomeCompany/HomeCompany.js
+++ src/componentes/HomeCompany/HomeCompany.js
@@ -1,12 +1,15 @@
 import { ContainerCarousel, ContainerCompany, ContainerPubli, Title } from "./style";
+import CompanyCarousel from "../CompanyCarousel/CompanyCarousel";
 
 function HomeCompany(){ 
+
+    
     return(
         <ContainerCompany>
             <Title>Faça parte da nossa rede de</Title>
             <Title>oportunidades</Title>
             <ContainerCarousel>
-                carrossel
+               <CompanyCarousel/>
             </ContainerCarousel>
             <ContainerPubli>
                 publicações
