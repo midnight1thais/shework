77856606dbfcfd84ee836e0b082b4d83b58584c1
Thais <thaisplaneta2017@gmail.com>
2023-08-28 12:24:12 -0300

    tirei o header da pagina home

--- src/pagina/Home/Home.js
+++ src/pagina/Home/Home.js
@@ -1,13 +1,12 @@
 import HomeCompany from "../../componentes/HomeCompany/HomeCompany"
 import HomeCover from "../../componentes/HomeCover/HomeCover"
-import HomeHeader from "../../componentes/HomeHeader/HomeHeader"
+// import HomeHeader from "../../componentes/HomeHeader/HomeHeader"
 import HomePerson from "../../componentes/HomePerson/HomePerson"
 
 function Home(){
     return(
         <>
         
-            <HomeHeader/>
             <HomeCover/>
             <HomeCompany/>
             <HomePerson/>
