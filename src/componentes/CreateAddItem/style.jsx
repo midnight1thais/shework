38bb6a4b11ef06cdb132c09d47d94c292474b4e9
Thais <thaisplaneta2017@gmail.com>
2023-08-31 23:22:17 -0300

    estido do createadditem criado

---
+++ src/componentes/CreateAddItem/style.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const CompetenciaContainer = styled.div`
+  background-color: #f2f2f2;
+  padding: 10px;
+  margin: 5px 0;
+`;
+
+export const CompetenciaDescricao = styled.p`
+  margin: 0;
+`;
