110e45ff626388d86794ed7bc9151c103f8d7392
Thais <thaisplaneta2017@gmail.com>
2023-09-10 13:50:42 -0300

    componente da tela da empresa criado

---
+++ src/componentes/CorpoAboutImg/CorpoAboutImg.js
@@ -0,0 +1,20 @@
+import { ContainerAboutLeft, ContainerAboutRight, InputRight } from "./style";
+
+
+function CorpoAboutImg(){
+
+    return(
+        <>
+        <ContainerAboutLeft>
+           <InputRight>
+            
+           </InputRight>
+        </ContainerAboutLeft>
+        <ContainerAboutRight>
+
+        </ContainerAboutRight>
+        </>
+    )
+}
+
+export default CorpoAboutImg
---
+++ src/componentes/CorpoAboutImg/style.jsx
@@ -0,0 +1,19 @@
+import { styled } from "styled-components";
+
+
+export const ContainerAboutLeft = styled.div`
+
+`
+
+export const ContainerAboutRight= styled.div`
+
+`
+
+export const InputRight = styled.div`
+    width: 527px;
+    height: 491px;
+    flex-shrink: 0;
+
+    background-color: #B6B8BA;
+`
+
