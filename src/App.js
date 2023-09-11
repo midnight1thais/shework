aaced5b4542e549fc43af2ca3aa8c064ad4d930e
Thais <thaisplaneta2017@gmail.com>
2023-09-10 23:52:58 -0300

    Rotas está no app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import StyledGlobal from "./styledGlobal";
 import { ChakraProvider } from '@chakra-ui/react'
+import Rotas from './rotas/Rotas';
 
 // import Login from './pagina/Login/Login';
 // import Register from './pagina/Register/Register';
 // import Home from './pagina/Home/Home'
+// import HomeRegister from './pagina/HomeRegister/HomeRegister';
+// import CreateJob from './pagina/CreateJob/CreateJob';
 // import CreatePubli from './pagina/CreatePubli/CreatePubli';
 // import PubliPerson from './pagina/PubliPerson/PubliPerson';
 // import JobCompany from './pagina/JobCompany/JobCompany';
 // import PubliCompany from './pagina/PubliCompany/PubliCompany';
-import Corporation from './pagina/Corporation/Corporation';
+// import Corporation from './pagina/Corporation/Corporation';
 
 function App() {
   return (
@@ -19,11 +22,14 @@ function App() {
         {/* <PubliCompany/> */}
         {/* <CreatePubli/> */}
         {/* <Home/> */}
+        {/* <HomeRegister/> */}
         {/* <Register /> */}
         {/* <Login/> */}
         {/* <PubliPerson/> */}
         {/* <JobCompany/> */}
-        <Corporation/>
+        {/* <Corporation/> */}
+        {/* <CreateJob/> */}
+        <Rotas/>
       </ChakraProvider>
     </>
   );
