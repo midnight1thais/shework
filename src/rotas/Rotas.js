68eb4ccbe3249df35bc30049827987c797ce0557
Thais <thaisplaneta2017@gmail.com>
2023-09-10 23:56:41 -0300

    rotas

---
+++ src/rotas/Rotas.js
@@ -0,0 +1,35 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from '../pagina/Home/Home'
+import Login from '../pagina/Login/Login'
+import HomeRegister from "../pagina/HomeRegister/HomeRegister";
+import Register from "../pagina/Register/Register";
+import ExploreCompany from '../pagina/ExploreCompany/ExploreCompany'
+import ExplorePerson from '../pagina/ExplorePerson/ExplorePerson'
+import CreatePubli from '../pagina/CreatePubli/CreatePubli'
+import CreateJob from '../pagina/CreateJob/CreateJob'
+import Corporation from '../pagina/Corporation/Corporation'
+import JobCompany from '../pagina/JobCompany/JobCompany'
+import PubliPerson from '../pagina/PubliPerson/PubliPerson'
+
+function Rotas(){
+
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route index element={<Home/>}/>
+            <Route path="login" element={<Login/>}/>
+            <Route path="homeRegister" element={<HomeRegister/>}/>
+            <Route path="register" element={<Register/>}/>
+            <Route path="exploreCompany" element={<ExploreCompany/>}/>
+            <Route path="explorePerson" element={<ExplorePerson/>}/>
+            <Route path="createPubli" element={<CreatePubli/>}/>
+            <Route path="createJob" element={<CreateJob/>}/>
+            <Route path="corporation" element={<Corporation/>}/>
+            <Route path="jobCompany" element={<JobCompany/>}/>
+            <Route path="publiPerson" element={<PubliPerson/>}/>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Rotas
