831d6feff8f6e01d0b5875fdfce47ed39f1ae43a
Thais <thaisplaneta2017@gmail.com>
2023-11-08 11:59:11 -0300

    modal com o children

--- frontend/src/componentes/CreateModal/CreateModal.js
+++ frontend/src/componentes/CreateModal/CreateModal.js
@@ -1,45 +1,16 @@
 import React, {useState} from 'react'
 import { Background, ContainerMid, ContainerModal, ContainerTop, Input, Label, TitleDes} from './style'
 
-function CreateModal({ isOpen, setModalOpen, addCurso, tipoLista}){
-  console.log('função', tipoLista)  
-
+function CreateModal({ isOpen, children}){
   const [nomeCurso, setNomeCurso] = useState(''); // Estado para o nome do curso
  
 
   if (isOpen) {
   
-      // Função para lidar com a adição do curso
-    const handleAddCurso = () => {
-      if (nomeCurso.trim() !== '') {
-          addCurso(nomeCurso, tipoLista);
-          console.log('handleAddCurso', tipoLista)
-          setModalOpen();
-      }
-    };
     return (
       <Background>
         <ContainerModal>
-          <div>
-            <ContainerTop>
-              <TitleDes> Adicionar Informação </TitleDes>
-            </ContainerTop>
-            <Label>Nome
-                  <Input type="text" value={nomeCurso} onChange={(e) => setNomeCurso(e.target.value)} />
-            </Label>
-            <ContainerMid>
-              <Label>Data de Inicio
-                    <Input type="text" />
-              </Label>
-              <Label>Data de Término
-                    <Input type="text" />
-              </Label>
-            </ContainerMid>
-            <Label>Descrição
-                    <Input type="text" />
-            </Label>
-          </div>
-          <button onClick={handleAddCurso}>Fechar</button>
+          <div>{children}</div>
         </ContainerModal>
       </Background>
     )
