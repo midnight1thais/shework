96e34eb68d9e4d29a53abbf88280792687077d85
Thais <thaisplaneta2017@gmail.com>
2023-11-15 16:37:16 -0300

    iniciei o processo de edição do usuario

--- frontend/src/componentes/PersonModal/PersonModal.js
+++ frontend/src/componentes/PersonModal/PersonModal.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Background, ButtonHere, ContainerButtons, ContainerMid, ContainerModal, ContainerTop, Input, Label, TitleDes} from './style'
 import { useNavigate } from 'react-router-dom'
+import { api } from "../../services/api";
+import axios from 'axios';
 
 function PersonModal({ isOpen, setModalOpen}){
+
+  const [nome, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setPassword] = useState("");
+
+  const [user, setUser] = useState("");
+  const userId= localStorage.getItem('@Auth:user_id')
 
   const navigate = useNavigate();
 
@@ -10,11 +19,52 @@ function PersonModal({ isOpen, setModalOpen}){
     // Limpa o localStorage
     localStorage.removeItem("@Auth:user");
     localStorage.removeItem("@Auth:token");
+    localStorage.removeItem("@Auth:email");
+    localStorage.removeItem("@Auth:id");
+    localStorage.removeItem("@Auth:user_id");
+    localStorage.removeItem("@Auth:user_categoria");
+    localStorage.removeItem("@Auth:user_name");
 
     // Redireciona de volta para a tela de login
     navigate("/login");
     window.location.reload();
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+
+    const data = {
+      nome,
+      email,
+      senha,
+      id_usuario:userId
+    };
+ 
+
+        const response = await api.put(`/user/${userId}`, data)
+            setUser(response.data);
+            
+            console.log(response.data.data[0])
+            localStorage.removeItem("@Auth:email");
+            localStorage.removeItem("@Auth:user_name");
+            if (response.data.success === true) {
+              alert("Usuário editado com sucesso!");
+            
+              const userName = response.data.data[0].nome;
+              const userEmail = response.data.data[0].email;
+
+              localStorage.setItem("@Auth:user_name", userName);
+              localStorage.setItem("@Auth:user_email", userEmail);
+
+            } else {
+              alert("Erro ao editar localStorage");
+            }
+            
+  };
+
+
+
 
   if (isOpen) {
 
@@ -27,19 +77,29 @@ function PersonModal({ isOpen, setModalOpen}){
             </ContainerTop>
             <ContainerMid>
               <Label>Nome
-                    <Input type="text" />
+                    <Input type="text" 
+                      value={nome}
+                      onChange={(e) => setName(e.target.value)}
+                    />
               </Label>
               <Label>Email
-                    <Input type="text" />
+                    <Input type="text" 
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)} 
+                    />
               </Label>
               <Label>Senha
-                    <Input type="text" />
+                    <Input type="password" 
+                      value={senha}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
               </Label>
             </ContainerMid>
            
           </div>
           <ContainerButtons>
             <ButtonHere onClick={setModalOpen}>Fechar</ButtonHere>
+            <ButtonHere onClick={handleSubmit}>Salvar</ButtonHere>
             <ButtonHere onClick={handleLogout}>Sair</ButtonHere>
           </ContainerButtons>
         </ContainerModal>
