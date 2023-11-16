e776dc824eedea59e2ab80add3d5daa92b401f14
Thais <thaisplaneta2017@gmail.com>
2023-11-15 21:14:55 -0300

    aumentei o texto do nameprofile da empresa

--- frontend/src/componentes/HomeRegisterCompanyHeader/style.jsx
+++ frontend/src/componentes/HomeRegisterCompanyHeader/style.jsx
@@ -45,8 +45,10 @@ export const Description = styled.div`
 
 
 export const NameProfile = styled.button`
+    display: flex;
+    align-items: left;
     margin-top: 1vh;
-    width: 11vw;
+    width: 12vw;
     color: #FFF;
     font-family: 'Poppins';
     font-size: 90%;
