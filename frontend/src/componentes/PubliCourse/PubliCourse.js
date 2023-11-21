7533b4be652b26650e8ec64f459c030e83a5b2ca
Thais <thaisplaneta2017@gmail.com>
2023-11-20 21:03:40 -0300

    listando cursos na tela

--- frontend/src/componentes/PubliCourse/PubliCourse.js
+++ frontend/src/componentes/PubliCourse/PubliCourse.js
@@ -1,28 +1,32 @@
 import { DivIcon, DivCourse, HeaderCourse, IconProfile, SectionCourse, TextCourse } from "./style";
 import PubliCourseIcon from '../../assets/PubliCourseIcon.svg'
 import PubliCourseCard from "../PubliCourseCard/PubliCourseCard";
+import { api } from "../../services/api";
+import { useEffect, useState } from "react";
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
-function PubliCourse(){
 
-    const divs = [
-        { date: "2021-2023", 
-        title:"Nome do Curso", 
-        descricao: "DESCRIÇÃO REDUZIDA" 
-        },
-        { date: "2021-2023", 
-        title:"Nome do Curso", 
-        descricao: "DESCRIÇÃO REDUZIDA" 
-        },
-        { date: "2021-2023", 
-        title:"Nome do Curso", 
-        descricao: "DESCRIÇÃO REDUZIDA" 
-        },
-        { date: "2021-2023", 
-        title:"Nome do Curso", 
-        descricao: "DESCRIÇÃO REDUZIDA"
-        },
-        // Add more items as needed
-      ];
+function PubliCourse({idPubliPerson}){
+
+    const [cursos, setCursos] = useState([]);
+    
+    useEffect(() => {
+        async function fetchPublis() {
+            try {
+                const response = await api.get(`/cursos/` + idPubliPerson); 
+                setCursos(response.data.data);
+                console.log(response.data.data)
+                
+            } catch (error) {
+                console.error('Erro ao recuperar as informações da publi:', error);
+            }
+        }
+
+        fetchPublis();
+    }, [idPubliPerson]);
+
+
 
     return(
         <>
@@ -34,16 +38,19 @@ function PubliCourse(){
                 <TextCourse>Cursos</TextCourse>
             </HeaderCourse>
             <DivCourse>
-            {divs.map((card) => (
+            {cursos.map((curso) => {  
+                const formattedDate = format(new Date(curso.dt_inicio), 'dd/MM/yyyy', { locale: ptBR });
+                const formattedDate2 = format(new Date(curso.dt_final), 'dd/MM/yyyy', { locale: ptBR });
+                return(
                 <PubliCourseCard
-                key={card}
-                date={card.date}
-                service={card.service}
-                title={card.title}
-                descricao={card.descricao}
+                key={curso}
+                dt_inicio={formattedDate}
+                dt_final={formattedDate2}
+                title={curso.nome}
+                descricao={curso.descricao}
                 isVisible={true}
             />
-            ))}
+            )})}
             </DivCourse>
 
         </SectionCourse>
