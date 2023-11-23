601beb831e720a17564b08a4812b91026e911924
Thais <thaisplaneta2017@gmail.com>
2023-11-23 20:28:35 -0300

    utilizando o link do projeto no card

--- frontend/src/componentes/JobProjectCard/JobProjectCard.js
+++ frontend/src/componentes/JobProjectCard/JobProjectCard.js
@@ -3,10 +3,12 @@ import { ContainerCard, Icon, Text } from "./style";
 
 function JobProjectCard({ altDes, iconSrc, text, isVisible }) {
   return (
+    <a href={altDes} rel="noreferrer" target="_blank">
     <ContainerCard isVisible={isVisible}>
-      <Icon src={iconSrc} alt={`${altDes} logo`} />
+      <Icon src={iconSrc} alt={`logo`} />
       <Text>{text}</Text>
     </ContainerCard>
+    </a>
   );
 }
 
