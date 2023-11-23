a845b821f192644c003c249e94c8f5d3dec44840
Thais <thaisplaneta2017@gmail.com>
2023-11-23 20:36:53 -0300

    adicionei get e mudei id dos parametro

--- backend/src/routes/publipersonRouter.js
+++ backend/src/routes/publipersonRouter.js
@@ -6,6 +6,7 @@ const router = Router();
 // Importar as funções (processamento da requisição) do controller
 const { 
     // da publicação pessoa
+    listAllPerson,
     listPerson,
     storePerson,
     updatePerson,
@@ -28,35 +29,38 @@ const {
     storeCertificados,
 
     listCursos,
-    storeCursos
+    storeCursos,
+    getPerson
 } = require('../controllers/publipersonController')
 
 // Criar os endpoints (rotas) que serão acessados a partir dos métodos HTTP (get,post,put,delete)
 
 // Da pessoa
-router.get('/publiperson/:id_user', listPerson);
+router.get('/publiperson/list', listAllPerson);
+router.get('/publipersonGet/:id_usuario', getPerson);
+router.get('/publiperson/:id_publicacao_pessoa', listPerson);
 router.post('/publiperson/create', storePerson);
 // router.put('/publiperson/:id_publiPessoa', updatePerson);
 // router.delete('/publiperson/:id', deletePerson);
 
 // Das competencias técnicas
-router.get('/competenciaTec/:id_user', listCompetenciaTec);
+router.get('/competenciaTec/:id_publicacao_pessoa', listCompetenciaTec);
 router.post('/competenciaTec/create', storeCompetenciaTec);
 
 // Das experiencias
-router.get('/experiencias/:id_user', listExperiencias);
+router.get('/experiencias/:id_publicacao_pessoa', listExperiencias);
 router.post('/experiencias/create', storeExperiencias);
 
 // Das linguas
-router.get('/linguas/:id_user', listLinguas);
+router.get('/linguas/:id_publicacao_pessoa', listLinguas);
 router.post('/linguas/create', storeLinguas);
 
 // Das certificados
-router.get('/certificados/:id_user', listCertificados);
+router.get('/certificados/:id_publicacao_pessoa', listCertificados);
 router.post('/certificados/create', storeCertificados);
 
 // Dos cursos
-router.get('/cursos/:id_user', listCursos);
+router.get('/cursos/:id_publicacao_pessoa', listCursos);
 router.post('/cursos/create', storeCursos);
 
 
