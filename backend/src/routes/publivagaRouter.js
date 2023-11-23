17b7d3ca57e11f716847a062ad3bb3510f4b6390
Thais <thaisplaneta2017@gmail.com>
2023-11-23 20:36:37 -0300

    mudei o id de parametro para id_publicacao_vaga

--- backend/src/routes/publivagaRouter.js
+++ backend/src/routes/publivagaRouter.js
@@ -28,27 +28,27 @@ const {
 // Criar os endpoints (rotas) que serão acessados a partir dos métodos HTTP (get,post,put,delete)
 
 // Da pessoa
-router.get('/publiVagaList/:id_user', listVaga);
+router.get('/publiVagaList/:id_publicacao_vaga', listVaga);
 router.post('/create', storeVaga);
 
 //Da progressao
-router.get('/listProgressao/:id_user', listProgressao);
+router.get('/listProgressao/:id_publicacao_vaga', listProgressao);
 router.post('/progressao/create', storeProgressao);
 
 //Dos requisitos obrigatórios
-router.get('/listRequisitosOB/:id_user', listRequisitosOB);
+router.get('/listRequisitosOB/:id_publicacao_vaga', listRequisitosOB);
 router.post('/requisitosOB/create', storeRequisitosOB);
 
 //Dos requisitos desejaveis
-router.get('/listRequisitosDJ/:id_user', listRequisitosDJ);
+router.get('/listRequisitosDJ/:id_publicacao_vaga', listRequisitosDJ);
 router.post('/requisitosDJ/create', storeRequisitosDJ);
 
 //Das responsabilidades
-router.get('/listResponsabilidades/:id_user', listResponsabilidades);
+router.get('/listResponsabilidades/:id_publicacao_vaga', listResponsabilidades);
 router.post('/responsabilidades/create', storeResponsabilidades);
 
 //Dos projetos
-// router.get('/listProjetos/:id_publicacao_vaga', listProjetos);
+router.get('/listProjetos/:id_publicacao_vaga', listProjetos);
 router.post('/projetos/create', storeProjetos);
 
 module.exports = router;
