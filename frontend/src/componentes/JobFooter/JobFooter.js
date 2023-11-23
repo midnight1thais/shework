ba2d5e2182df0a8780496337f93cba5122c7249d
Thais <thaisplaneta2017@gmail.com>
2023-11-23 20:31:09 -0300

    validação para o botão da aplicação para a vaga

--- frontend/src/componentes/JobFooter/JobFooter.js
+++ frontend/src/componentes/JobFooter/JobFooter.js
@@ -4,12 +4,20 @@ import { Link } from "react-router-dom";
 
 function JobFooter(){
 
+  // Supondo que a categoria está armazenada no localStorage como "category"
+  const categoria = localStorage.getItem("@Auth:user_categoria");
+
+  // Verifica se a categoria é "Trabalhos Diversos" ou "Técnico em TI"
+  const exibirBotaoAplicar = categoria === "Trabalhos Diversos" || categoria === "Técnico em TI";
+
     return(
         <>
         <ButtonBack>
-            <Link to='/homeRegister'><BackIcon src={IconButtonBack} alt=""/></Link>
+            <Link to='/homeRegisterCompany'><BackIcon src={IconButtonBack} alt=""/></Link>
         </ButtonBack>
+        {exibirBotaoAplicar && (
         <Link to='/exploreCompany'><ButtonApply>Aplique para a Vaga</ButtonApply></Link>
+        )}
         <div>
         </div>
         </>
