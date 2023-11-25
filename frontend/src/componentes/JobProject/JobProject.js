123d7527a59ebcec4268a548f4139bfc4e1c1eeb
Thais <thaisplaneta2017@gmail.com>
2023-11-25 16:45:48 -0300

    projetos da vaga

--- frontend/src/componentes/JobProject/JobProject.js
+++ frontend/src/componentes/JobProject/JobProject.js
@@ -1,68 +1,76 @@
 import React, { useEffect, useState } from "react";
+import JobRespoIcon from '../../assets/JobCourseIcon.png'
 import JobProjectCard from "../JobProjectCard/JobProjectCard";
 import IconProject from "../../assets/JobProjectIcon.svg";
 import { ArrowBack, ArrowNext, ButtonBack, ButtonNext, ContainerCards, ContainerCarousel } from "./style";
 import seta from '../../assets/seta.svg'
 import { api } from "../../services/api";
+import { DivIcon, HeaderInfo, IconProfile, TextInfo } from "../JobProgression/style";
 
 function JobProject({idJobCompany}) {
-
-  const [projects, setProject] = useState([]);
-    
-  useEffect(() => {
-      async function fetchPublis() {
-          try {
-              const response = await api.get(`/publivaga/listProjetos/` + idJobCompany); 
-              setProject(response.data.data);
-              console.log("resposta do projetos:", response.data.data)
-              
-          } catch (error) {
-              console.error('Erro ao recuperar as informações da publi:', error);
-          }
-      }
-
-      fetchPublis();
-  }, [idJobCompany]);
-
+  const [projects, setProjects] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        const response = await api.get(`/publivaga/listProjetos/` + idJobCompany);
+        setProjects(response.data.data);
+        console.log("resposta do projetos:", response.data.data);
+      } catch (error) {
+        console.error('Erro ao recuperar as informações dos projetos:', error);
+      }
+    }
+
+    fetchProjects();
+  }, [idJobCompany]);
+
+  const showNavigationButtons = projects.length > 3; // Se houver mais de 3 projetos, mostra os botões.
+
   const handleClickNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === projects.length - 1 ? 0 : prevIndex + 1
-    );
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projects.length);
   };
 
   const handleClickPrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? projects.length - 1 : prevIndex - 1
-    );
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? projects.length - 1 : prevIndex - 1));
   };
 
   return (
     <>
-    {projects.length > 0 && (
-    <ContainerCarousel>
-      <ButtonBack onClick={handleClickPrev}>
-        <ArrowBack src={seta} alt=''/>
-      </ButtonBack>
-      <ContainerCards>
-        {[currentIndex, (currentIndex + 1) % projects.length, (currentIndex + 2) % projects.length].map((cardIndex) => (
-          <JobProjectCard
-            key={cardIndex}
-            altDes={projects[cardIndex].projeto_link}
-            iconSrc={IconProject}
-            text={projects[cardIndex].descricao}
-            isVisible={true}
-          />
-        ))}
-      </ContainerCards>
-      <ButtonNext onClick={handleClickNext}>
-        <ArrowNext src={seta} alt=''/>
-      </ButtonNext>
-    </ContainerCarousel>
-    )}
+      <HeaderInfo>
+          <DivIcon>
+              <IconProfile src={JobRespoIcon} alt=""/>
+          </DivIcon>
+          <TextInfo>Projetos Relacionados</TextInfo>
+      </HeaderInfo>
+      {projects.length > 0 && (
+        <ContainerCarousel>
+          {showNavigationButtons && (
+            <ButtonBack onClick={handleClickPrev}>
+              <ArrowBack src={seta} alt='' />
+            </ButtonBack>
+          )}
+          <ContainerCards>
+            {projects.map((project, index) => (
+              <JobProjectCard
+                key={index}
+                altDes={project.projeto_link}
+                iconSrc={IconProject}
+                text={project.descricao}
+                isVisible={index === currentIndex}
+              />
+            ))}
+          </ContainerCards>
+          {showNavigationButtons && (
+            <ButtonNext onClick={handleClickNext}>
+              <ArrowNext src={seta} alt='' />
+            </ButtonNext>
+          )}
+        </ContainerCarousel>
+      )}
     </>
   );
 }
+
 
 export default JobProject;
