5dce4f143504f03a2a2405e27e5e759c633b1040
Thais <thaisplaneta2017@gmail.com>
2023-11-25 16:47:53 -0300

    card das oportunidade de vagas recebendo as informacoes necessarias

--- frontend/src/componentes/CorpoOpportunitiesCard/CorpoOpportunitiesCard.js
+++ frontend/src/componentes/CorpoOpportunitiesCard/CorpoOpportunitiesCard.js
@@ -1,33 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImgShare from '../../assets/compartilhar.svg'
 import ImgContract from '../../assets/contratar.svg'
 import { ButtonRight, ContainerAll, ContainerCard, ContainerImg, ContainerTop, Date, Description, IconRight, Service, Title } from "./style";
+import { api } from "../../services/api";
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
-function CorpoOpportunitesCard() {
+function CorpoOpportunitesCard({nomeVaga, tipoVaga, areaVaga, dataVaga}) {
     
-    const isVisible = true;
+    const isVagaVisible = true;
 
-    const cardArray = new Array(5).fill(null);
   
     return (
     <>
     <div>
-    {cardArray.map((_, index) => (
-    <ContainerCard key={index} isVisible={isVisible}>
+    <ContainerCard isVisible={isVagaVisible}>
         <ContainerAll>
             <ContainerTop>
-            <Date>21 Julho 2023</Date>
-            <Service>Serviços Gerais</Service>
+            <Date>{dataVaga}</Date>
+            <Service>{tipoVaga}</Service>
             </ContainerTop>
-            <Title>Andressa de Lima</Title>
-            <Description>Tenho 21 anos e estou procurando vaga para técnico em TI , faço programas em ReactJS...</Description>
+            <Title>{nomeVaga}</Title>
+            <Description>{areaVaga}</Description>
         </ContainerAll>
         <ContainerImg>
-            <ButtonRight><IconRight src={ImgShare} alt=''/></ButtonRight>
             <ButtonRight><IconRight src={ImgContract} alt=''/></ButtonRight>
         </ContainerImg>
     </ContainerCard>
-  ))}
     </div>
     </>
   );
