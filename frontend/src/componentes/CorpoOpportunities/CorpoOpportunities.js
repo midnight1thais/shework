99355120fa9be19632ee63d8913e749d9ada02b2
Thais <thaisplaneta2017@gmail.com>
2023-11-25 16:46:10 -0300

    Listagem das oportunidades de vaga

--- frontend/src/componentes/CorpoOpportunities/CorpoOpportunities.js
+++ frontend/src/componentes/CorpoOpportunities/CorpoOpportunities.js
@@ -1,7 +1,31 @@
 import { ContainerText, DivCourse,  SectionCourse, TextRight } from "./style";
 import CorpoOpportunitesCard from "../CorpoOpportunitiesCard/CorpoOpportunitiesCard";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
-function CorpoOpportunities(){
+function CorpoOpportunities({idCorporation}){
+
+        // useState para publicacoes 
+        const [infosVaga, setInfosVaga] = useState([]);
+    
+        //get das pessoas
+    
+        useEffect(() => {
+            async function fetchPublis() {
+                try {
+                    const response = await api.get(`/publivaga/publiCompanyVaga/${idCorporation}`); 
+                    setInfosVaga(response.data.data);
+                    console.log("resposta do response.data DO CARD DA VAGA:", response.data.data)
+                    
+                } catch (error) {
+                    console.error('Erro ao recuperar as informações da vaga:', error);
+                }
+            }
+        
+            fetchPublis();
+        }, []);
 
     return(
         <>
@@ -10,7 +34,19 @@ function CorpoOpportunities(){
         </ContainerText>
         <SectionCourse>
             <DivCourse>
-                <CorpoOpportunitesCard />
+            {infosVaga.map((infos) => {  
+                const formattedDate = format(new Date(infos.dt_anuncio), 'dd/MM/yyyy', { locale: ptBR });
+                console.log('informações da vaga', infos)
+                return(
+                <CorpoOpportunitesCard 
+                idCompany={idCorporation}
+                nomeVaga={infos.nome_vaga}
+                tipoVaga={infos.tipo_vaga}
+                areaVaga={infos.area_atuacao}
+                dataVaga={formattedDate}
+                />
+                    )
+                })}
             </DivCourse>
         </SectionCourse>
         
