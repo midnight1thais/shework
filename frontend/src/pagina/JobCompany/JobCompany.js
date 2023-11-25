fb2b89e849d6986b3c4098587562dfacc348df34
Thais <thaisplaneta2017@gmail.com>
2023-11-25 16:34:03 -0300

    Lista o nome da vaga correto

--- frontend/src/pagina/JobCompany/JobCompany.js
+++ frontend/src/pagina/JobCompany/JobCompany.js
@@ -6,16 +6,40 @@ import JobProject from "../../componentes/JobProject/JobProject";
 import JobRequisite from "../../componentes/JobRequisite/JobRequisite";
 import JobRespo from "../../componentes/JobRespo/JobRespo";
 import { DivDetails, DivFooter, DivHeader, DivProgression, DivProject, DivRequisite, DivRespo, HeaderText, SectionGlobal } from "./style";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 
 function JobCompany() {
 
     const { id_job } = useParams();
 
+    const [infos, setInfos] = useState([]);
+    
+    
+    useEffect(() => {
+        async function fetchPublis() {
+            try {
+                const response = await api.get(`/publivaga/publiVagaList/` + id_job); 
+                setInfos(response.data.data);
+                
+            } catch (error) {
+                console.error('Erro ao recuperar as informações da publi:', error);
+            }
+        }
+  
+        fetchPublis();
+    }, [id_job]);
+
     return(
         <SectionGlobal>
+            {infos.map((item) => {
+                return (
             <DivHeader>
-                <HeaderText> NOME DO CARGO DA EMPRESA </HeaderText>
+                <HeaderText> {item.nome_vaga} </HeaderText>
             </DivHeader>
+                    )
+                    
+                })}
             <DivDetails>
                 <JobInfo idJobCompany={id_job}/>
             </DivDetails>
