c135d7aac317bf44b80cb64720f2416357c29ef5
Thais <thaisplaneta2017@gmail.com>
2023-11-28 10:26:54 -0300

    validacao do botão para publicar vaga

--- frontend/src/componentes/CompanyPubli/CompanyPubli.js
+++ frontend/src/componentes/CompanyPubli/CompanyPubli.js
@@ -1,8 +1,31 @@
 import { ButtonPubli, ContainerLink, ContainerTitle, LinkA, TitleNormal, TitlePurple, CompanyText, ContainerLeft, ContainerRight } from "./style";
 import CompanyImg from '../../assets/CompanyImg.svg'
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function CompanyPubli(){
+
+    const [enable, setEnable] = useState(true);
+    const categoria = localStorage.getItem('@Auth:user_categoria')
+    
+    useEffect(() => {
+        async function fetchPublis() {
+            try {
+                if (categoria === 'Técnico em TI' || categoria === 'Trabalhos Diversos' || !categoria){
+                    console.log('ele é uma pessoa e não pode publicar como empresa')
+                    setEnable(false)
+                } else{
+                  console.log('ele é uma empresa')
+                  setEnable(true)
+                }
+                
+            } catch (error) {
+                console.error('Erro ao recuperar as informações da publi:', error);
+            }
+        }
+  
+        fetchPublis();
+    }, [categoria]);
 
     return(
         <>
@@ -16,10 +39,12 @@ function CompanyPubli(){
                 <TitlePurple>profissional!</TitlePurple>
             </ContainerTitle>
             <ButtonPubli><Link to='/exploreCompany'>Explore as publicações</Link></ButtonPubli>
+            {enable &&
             <ContainerLink>
                 <CompanyText>É uma empresa?</CompanyText>
                 <LinkA><Link to='/createJob'>Publique uma VAGA</Link></LinkA>
             </ContainerLink>
+            }
         </ContainerLeft>
         <ContainerRight>
             <img src={CompanyImg} alt="Uma pessoa tecnologica"/>
