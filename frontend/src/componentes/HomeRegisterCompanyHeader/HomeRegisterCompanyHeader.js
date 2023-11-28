470e1d50236c7002c847587973ede6814a795aed
Thais <thaisplaneta2017@gmail.com>
2023-11-28 10:21:28 -0300

    para setar a publiCompany_id se ela tiver publi

--- frontend/src/componentes/HomeRegisterCompanyHeader/HomeRegisterCompanyHeader.js
+++ frontend/src/componentes/HomeRegisterCompanyHeader/HomeRegisterCompanyHeader.js
@@ -1,14 +1,56 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { HeaderGlobal, Menu, ItemMenu, LogoStyle, Description, NameProfile, DesProfile, ProfileDiv } from './style'
 import Logo from '../../assets/Logo.svg'
 import HomeRegisterProfile from '../HomeRegisterProfile/HomeRegisterProfile';
 import CompanyModal from '../CompanyModal/CompanyModal';
+import { api } from '../../services/api';
 
 function HomeRegisterCompanyHeader() {
 
     const [openModal, setOpenModal] = useState(false);
     const userIdName = localStorage.getItem('@Auth:user_name')
     const userIdCategoria = localStorage.getItem('@Auth:user_categoria')
+    const userId =  localStorage.getItem('@Auth:user_id')
+
+    const [infos, setInfos] = useState('');
+
+    useEffect(() => {
+        async function fetchPublis() {
+            try {
+                // const response = await api.get(`/publiperson/` + userId); 
+                // setInfos(response.data.data);
+
+                const response = await api.get(`/company/companyGet/${userId}`);
+                console.log('tentando pegar publicacao empresa', response.data);
+                    // Reorna da API com as chaves
+                    if (response.data.success === true) {
+                        const publiEmpresa = response.data.data[0].id_publiEmpresa;
+                        localStorage.setItem("@Auth:publiCompany_id", publiEmpresa); 
+                        console.log('companyGet', response.data.data[0].id_publiEmpresa);
+                    } else {
+                        console.log('deu problem')
+                    }  
+                
+            } catch (error) {
+                console.error('Erro ao recuperar as informações da publi:', error);
+            }
+        }
+  
+        fetchPublis();
+    }, [userId]);
+
+
+    // const response = await api.get(`/company/companyGet/${userId}`);
+    // console.log('tentando pegar publicacao empresa', response.data);
+    //     // Reorna da API com as chaves
+    //     if (response.data.success === true) {
+    //         // const publiEmpresa = response.data.data[0].id_publiEmpresa;
+    //         // localStorage.setItem("@Auth:publiCompany_id", publiEmpresa); 
+    //         // console.log('companyGet', response.data.data[0].id_publiEmpresa);
+    //         alert('entrou no true')
+    //     } else if (response.data.success === false) {
+    //         alert('te manda bro')
+    //     }  
 
     return(
         <>
