6a9834cf20355c3c58ae8b98632b86b2dc1aa6a1
Thais <thaisplaneta2017@gmail.com>
2023-11-28 10:17:03 -0300

    card da pessoa com as informacoes necessarias

--- frontend/src/componentes/PersonCard/PersonCard.js
+++ frontend/src/componentes/PersonCard/PersonCard.js
@@ -1,17 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ImgPersonCard from '../../assets/ImgPersonCard.png'
 import { ContainerAll, ContainerCard, ContainerTop, Date, Description, Icon, Service, Title } from "./style";
+import { useNavigate } from "react-router-dom";
 
-function PersonCard({ date, service, title, descricao, iconSrc, isVisible }) {
+function PersonCard({id_publicacao_pessoa, publiArea, publiNome, publiDescricao, iconSrc, isVisible }) {
+
+  const navigate = useNavigate();
+
+  const categoria = localStorage.getItem('@Auth:user_categoria')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+        try {
+          if (categoria === 'Técnico em TI' || categoria === 'Trabalhos Diversos' || categoria === 'Empresa' ){
+            const currentPath = window.location.pathname; // Pega o caminho atual
+      
+            // Método replace (função do JavaScript) para remover tanto /homeRegister quanto /homeRegisterCompany
+            const newPath = currentPath.replace(/\/?(homeRegister(Company)?|explorePerson)/, '');
+    
+            // Adiciona a nova parte ao caminho
+            navigate(`${newPath}/publiPerson/${id_publicacao_pessoa}`);
+          
+          } else{
+            console.log('ele não esta cadastrado')
+            navigate(`/publiPerson/${id_publicacao_pessoa}`);
+          }
+      
+      } catch (error) {
+          console.error('Erro ao tentar acessar publicacao', error);
+      }
+
+  };
+  
+  // useEffect(() => {
+  //     async function fetchPublis() {
+  //         try {
+  //             if (categoria === 'Técnico em TI' || categoria === 'Trabalhos Diversos' || !categoria){
+  //                 const currentPath = window.location.pathname; // Pega o caminho atual
+              
+  //                 // Método replace (função do JavaScript) para remover tanto /homeRegister quanto /homeRegisterCompany
+  //                 const newPath = currentPath.replace(/\/?(homeRegister(Company)?|explorePerson)/, '');
+              
+  //                 // Adiciona a nova parte ao caminho
+  //                 navigate(`${newPath}/publiPerson/${id_publicacao_pessoa}`);
+  
+                  
+  //             } else{
+  //               console.log('ele é uma empresa')
+  //             }
+              
+  //         } catch (error) {
+  //             console.error('Erro ao recuperar as informações da publi:', error);
+  //         }
+  //     }
+
+  //     fetchPublis();
+  // }, [categoria]);
+
+
+
   return (
-    <ContainerCard isVisible={isVisible}>
-      <Icon src={iconSrc} alt=''/>
+    <ContainerCard onClick={handleSubmit} isVisible={isVisible}>
+      <Icon src={ImgPersonCard} alt=''/>
       <ContainerAll>
         <ContainerTop>
-          <Date>{date}</Date>
-          <Service>{service}</Service>
+          <Date>2023</Date>
+          <Service>{publiArea}</Service>
         </ContainerTop>
-        <Title>{title}</Title>
-        <Description>{descricao}</Description>
+        <Title>{publiNome}</Title>
+        <Description>{publiDescricao}</Description>
       </ContainerAll>
     </ContainerCard>
   );
