d8c8fb5aac29d79916515a396887d7668c71cc9a
Thais <thaisplaneta2017@gmail.com>
2023-11-28 10:06:46 -0300

    pesquisa funciona, coloquei o navigate correto e modifiquei a questão do filtro

--- frontend/src/pagina/ExplorePerson/ExplorePerson.js
+++ frontend/src/pagina/ExplorePerson/ExplorePerson.js
@@ -4,17 +4,27 @@ import { BigTitle, ComboBox, ComboBoxContainer, ContainerCarousel, ContainerHead
 import IconBack from '../../assets/JobButtonBack.svg'
 import iconsearch from '../../assets/IconSearch.svg'
 import ExplorePersonCard from "../../componentes/ExplorePersonCard/ExplorePersonCard";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { api } from "../../services/api";
 
 function ExplorePerson(){
 
     // para a pesquisa
     const [searched, setSearched] = useState([]);
+    
 
     const handleSearchChange = (value) =>{
         setSearched(value);
     }
+
+    //para voltar
+
+    const navigate = useNavigate();
+
+    const goBack = () =>{
+      navigate(-1)
+    }
+  
 
     // useState para publicacoes 
     const [infosPubliPerson, setInfosPubliPerson] = useState([]);
@@ -26,7 +36,7 @@ function ExplorePerson(){
             try {
                 const response = await api.get(`/publiperson/list`); 
                 setInfosPubliPerson(response.data.data);
-                console.log("resposta do response.data :", response.data.data.id_publicacao_pessoa)
+                console.log("resposta do response categoria:", response.data.data)
                 
             } catch (error) {
                 console.error('Erro ao recuperar as informações da publi:', error);
@@ -61,6 +71,7 @@ function ExplorePerson(){
       setSelectedOption(event.target.value);
     };
 
+
     return(
         <>
         <ContainerHeader>
@@ -68,7 +79,7 @@ function ExplorePerson(){
             <LittleTitle>Compartilhe sua vaga para uma possível contratação</LittleTitle>
         </ContainerHeader>
 
-        <Link to='/homeRegister'><img src={IconBack} alt=""/></Link>
+        <button onClick={goBack}><img src={IconBack} alt=""/></button>
 
         <ContainerSearch>
             <ContainerInput>
@@ -90,26 +101,35 @@ function ExplorePerson(){
         </ComboBoxContainer>
         <ComboBoxContainer>
             <ComboBox value={selectedOption} onChange={handleOptionChange}>
-            <OptionSelected value="">Técnico em TI</OptionSelected>
-            {options2.map((option, index) => (
-                <OptionSelected key={index} value={option}>
-                {option}
-                </OptionSelected>
-            ))}
+            <OptionSelected value="">Tecnologia</OptionSelected>
+                <OptionSelected value="Técnico em TI">Técnico em TI</OptionSelected>
+                <OptionSelected value="Analista de sistemas de automação">Analista de sistemas de automação</OptionSelected>
+                <OptionSelected value="Desenhista de páginas da internet">Desenhista de páginas da internet</OptionSelected>
+                <OptionSelected value="Programador de multimídia">Programador de multimídia</OptionSelected>
+                <OptionSelected value="Monitorador de sistemas">Monitorador de sistemas</OptionSelected>
+                <OptionSelected value="Todos">Todos</OptionSelected>
             </ComboBox>
         </ComboBoxContainer>
         </ContainerSearch>
         <ContainerCarousel>
-            {infosPubliPerson.filter((infos) => 
+            {/* {infosPubliPerson.filter((infos) => 
             infos.nome.includes(searched))
+            .map((infos, index) => ( */}
+        
+        {infosPubliPerson
+            .filter((infos) => infos.nome.toLowerCase().includes(searched && typeof searched === 'string' ? searched.toLowerCase() : ''))
             .map((infos, index) => (
                 <ExplorePersonCard
-                key={index}
-                id_publicacao_pessoa={infos.id_publicacao_pessoa}
-                isVisible={true}
+                    key={index}
+                    id_publicacao_pessoa={infos.id_publicacao_pessoa}
+                    publiNome={infos.nome}
+                    publiArea={infos.area_interesse}
+                    publiDescricao={infos.descricao}
+                    isVisible={true}
                 />
             ))
-            }
+        }
+
         {/* {infosPubliPerson.map((infos) => {  
             console.log('Infos Object:', infos); 
                 return(
