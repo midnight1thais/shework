470b227319c9adbacc4872026b36576ef14f89ea
Thais <thaisplaneta2017@gmail.com>
2023-11-28 10:25:43 -0300

    card oportunidades completo

--- frontend/src/componentes/CorpoOpportunitiesCard/CorpoOpportunitiesCard.js
+++ frontend/src/componentes/CorpoOpportunitiesCard/CorpoOpportunitiesCard.js
@@ -5,16 +5,47 @@ import { ButtonRight, ContainerAll, ContainerCard, ContainerImg, ContainerTop, D
 import { api } from "../../services/api";
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import { useNavigate } from "react-router-dom";
 
-function CorpoOpportunitesCard({nomeVaga, tipoVaga, areaVaga, dataVaga}) {
+function CorpoOpportunitesCard({idVaga, nomeVaga, tipoVaga, areaVaga, dataVaga}) {
     
     const isVagaVisible = true;
 
+    const navigate = useNavigate();
+
+    const categoria = localStorage.getItem('@Auth:user_categoria')
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      
+          try {
+            if (categoria === 'Técnico em TI' || categoria === 'Trabalhos Diversos' || categoria === 'Empresa'){
+              const currentPath = window.location.pathname; // Pega o caminho atual
+        
+              // Método replace (função do JavaScript) para remover tanto /homeRegister quanto /homeRegisterCompany
+              
+              const newPath = `/jobCompany/${idVaga}`;
+              
+              // substituir a entrada do histórico em vez de adicionar uma nova entrada
+              navigate(newPath, { replace: true });
+            
+            } else{
+              console.log('ele não esta cadastrado')
+              const newPath = `/jobCompany/${idVaga}`;
+              // substituir a entrada do histórico em vez de adicionar uma nova entrada
+              navigate(newPath, { replace: true });
+            }
+        
+        } catch (error) {
+            console.error('Erro ao tentar acessar publicacao', error);
+        }
+  
+    };
   
     return (
     <>
     <div>
-    <ContainerCard isVisible={isVagaVisible}>
+    <ContainerCard onClick={handleSubmit} isVisible={isVagaVisible}>
         <ContainerAll>
             <ContainerTop>
             <Date>{dataVaga}</Date>
